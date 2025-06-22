01fe05f96512b4e19eaaaa925a9e6986795e26db
Natnael12119 <nhattydeveloper@gmail.com>
2025-06-22 13:41:48 -0400

    price added in the breakfast menu

--- restaurant_menu.js
+++ restaurant_menu.js
@@ -1,4 +1,5 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
@@ -13,4 +14,4 @@ document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 let dessertItem = '';
 for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
-document.getElementById('dessertMenuItem').innerHTML = dessertItem
+document.getElementById('dessertMenuItem').innerHTML = dessertItem;
